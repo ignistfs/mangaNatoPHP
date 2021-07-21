ab7807f41bd6bf6c9f0c9a41487b5e07a07ef1a8
herc <herc@herc.home.net>
2021-07-21 11:01:00 -0400

    added getMangaInfo

---
+++ tests/test2.php
@@ -0,0 +1,38 @@
+<center>
+  <h1>Test and demo for getMangaInfo method</h1>
+  <h2>searchManga will be used first to get a mangaid</h2>
+<form action="test2.php" method="GET">
+<input style="margin:5px;width:200px;;height:60px;" placeholder="Search manga by name" name="k" />
+
+</form>
+<?php
+if(!isset($_GET['k'])){
+  return;
+}
+
+require("../mangakakalotApi.php");
+
+
+$getmanga= new Kakalot;
+$getmanga->searchManga($_GET['k']); //search for mangaid with the search method
+$mangaid = $getmanga->getResult()['mangaid']; // get mangaid
+$getmanga->getMangaInfo($mangaid); //fetch manga info
+$MangaInfo=$getmanga->getResult();
+
+
+?>
+
+Query : <span style="color:green"><?php print($_GET['k']);?></span><br><br>
+Json Encoded response <br><br><textarea><?php print_r(json_encode($MangaInfo)) ?> </textarea><br><br>
+Html Result :
+<h2><?php print($MangaInfo['title'])?></h2><br>
+<h4><img src="<?php print($MangaInfo['cover'])?>"/></h4><br>
+<h4><?php print($MangaInfo['description'])?></h4><br>
+<?php
+
+foreach($MangaInfo['chapters'] as $chapter){
+  print("<a href='".$chapter['link']."'>".$chapter['title']."</a><br>");
+}
+
+
+?>
